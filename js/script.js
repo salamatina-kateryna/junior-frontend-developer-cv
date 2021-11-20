dfb7611f66c58d954d413ac9a6dd37fd64cf607c
LinusVillain <ksalamatina95@gmail.com>
2021-11-20 17:03:51 +0200

    Added smooth scroll.

--- js/script.js
+++ js/script.js
@@ -55,3 +55,15 @@ overlay.addEventListener(`click`, (evt) => {
         overlay.classList.add(`hidden`)
     }
 });
+
+// smooth scroll
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
